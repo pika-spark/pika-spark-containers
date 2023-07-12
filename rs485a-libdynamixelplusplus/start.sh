29dcf96f00f481af654506b5a857059ff5d9b056
Alexander Entinger <consulting@lxrobotics.com>
2023-07-12 13:09:31 +0200

    Adjust for default startup baudrate of dynamixel servos.

--- rs485a-libdynamixelplusplus/start.sh
+++ rs485a-libdynamixelplusplus/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH="$PATH:/tmp/libdynamixelplusplus/build/examples/example-01-ping"
 echo "Starting example-01-ping ..."
-example-01-ping /dev/ttymxc1 2000000
+example-01-ping /dev/ttymxc1 57600
