8265c29a184a92b198f637887aad8d2fa156127e
Alexander Entinger <consulting@lxrobotics.com>
2023-08-03 11:43:53 +0200

    Dump content on stdout into less, thereby enabled scrolling through the content of the EEPROM.

--- eeprom_dump/start.sh
+++ eeprom_dump/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 export PATH="$PATH:/tmp/portenta-x8-eeprom-utils"
 export PATH="$PATH:/tmp/portenta-x8-eeprom-utils/build"
-./eepflash.sh --read --device=1 --address=50 -t=24c256 -f dump.eep
+eepflash.sh --read --device=1 --address=50 -t=24c256 -f eeprom_dump.eep
+eepdump eeprom_dump.eep eeprom_dump.txt
+less eeprom_dump.txt
