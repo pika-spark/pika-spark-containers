ed6710288189012777448337dc2b7eb39779c1a2
Alexander Entinger <consulting@lxrobotics.com>
2023-08-03 11:31:21 +0200

    Specify commands to dump the EEPROM's concent into a file.

--- eeprom_dump/start.sh
+++ eeprom_dump/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export PATH="$PATH:/tmp/portenta-x8-eeprom-utils"
 export PATH="$PATH:/tmp/portenta-x8-eeprom-utils/build"
+./eepflash.sh --read --device=1 --address=50 -t=24c256 -f dump.eep
