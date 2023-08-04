3cc7ba60ac633e935f6ae719c34f6a0dda3c96a3
Alexander Entinger <consulting@lxrobotics.com>
2023-08-04 09:08:09 +0200

    Load AT24 kernel module before invoking docker.

--- eeprom-dump/docker-run.sh
+++ eeprom-dump/docker-run.sh
@@ -7,4 +7,12 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
+modprobe at24
+
+rc=$?
+if [ $rc != 0 ]; then
+	echo "Modprobe of at24 failed."
+	exit $rc
+fi
+
 docker run -it -u 0 --privileged -v /sys/class/i2c-dev:/sys/class/i2c-dev pika_spark_eeprom_dump
