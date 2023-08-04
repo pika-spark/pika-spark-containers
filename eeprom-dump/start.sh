d35c5488a9ff066cb78451621df10405c0337868
Alexander Entinger <consulting@lxrobotics.com>
2023-08-04 09:18:01 +0200

    Replace "less" with "cat" (less confusing for the user).

--- eeprom-dump/start.sh
+++ eeprom-dump/start.sh
@@ -2,4 +2,4 @@
 export PATH="$PATH:/tmp/portenta-x8-eeprom-utils"
 eepflash.sh --read --device=1 --address=50 -t=24c256 -f eeprom_dump.eep
 eepdump eeprom_dump.eep eeprom_dump.txt
-less eeprom_dump.txt
+cat eeprom_dump.txt
