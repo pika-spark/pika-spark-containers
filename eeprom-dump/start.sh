9138962a007ae0190c33827485b7faba7337b7ce
Alexander Entinger <consulting@lxrobotics.com>
2023-08-04 08:51:46 +0200

    eedump, eepmake are automatically installed in the system PATH.

--- eeprom-dump/start.sh
+++ eeprom-dump/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export PATH="$PATH:/tmp/portenta-x8-eeprom-utils"
-export PATH="$PATH:/tmp/portenta-x8-eeprom-utils/build"
 eepflash.sh --read --device=1 --address=50 -t=24c256 -f eeprom_dump.eep
 eepdump eeprom_dump.eep eeprom_dump.txt
 less eeprom_dump.txt
