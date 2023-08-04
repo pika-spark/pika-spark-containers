909fad257bb30af4a8a3f52e75e5c55fbe0b80b6
Alexander Entinger <consulting@lxrobotics.com>
2023-08-04 09:34:06 +0200

    Disable write-protect for EEPROM.

--- eeprom-flash/docker-run.sh
+++ eeprom-flash/docker-run.sh
@@ -15,4 +15,18 @@ if [ $rc != 0 ]; then
 	exit $rc
 fi
 
+GPIO_EEPROM_WRITE_PROTECT=88
+
+function finish
+{
+  echo 1 > /sys/class/gpio/gpio$GPIO_EEPROM_WRITE_PROTECT/value
+  echo $GPIO_EEPROM_WRITE_PROTECT > /sys/class/gpio/unexport
+}
+trap finish EXIT
+
+echo "Disable write protect for EEPROM"
+echo $GPIO_EEPROM_WRITE_PROTECT > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio$GPIO_EEPROM_WRITE_PROTECT/direction
+echo 0 > /sys/class/gpio/gpio$GPIO_EEPROM_WRITE_PROTECT/value
+
 docker run -it -u 0 --privileged -v /sys/class/i2c-dev:/sys/class/i2c-dev pika_spark_eeprom_flash
