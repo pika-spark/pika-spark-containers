9dc487b5e8b806bc57a912f62575f9d11f6bf96a
Alexander Entinger <consulting@lxrobotics.com>
2023-08-08 12:49:13 +0200

    Provide launch file to work for Pika Spark.

--- ros-l3xz-head-demo/demo.py
+++ ros-l3xz-head-demo/demo.py
@@ -4,8 +4,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    ros2_dynamixel_bridge_launch_dir = get_package_share_directory('ros2_dynamixel_bridge') + '/launch/bridge-l3xz.py'
-    ros2_dynamixel_bridge = IncludeLaunchDescription(PythonLaunchDescriptionSource(ros2_dynamixel_bridge_launch_dir))
+    ros2_dynamixel_bridge = IncludeLaunchDescription(PythonLaunchDescriptionSource('/tmp/colcon/demo-bridge.py'))
 
     l3xz_head_ctrl_launch_dir = get_package_share_directory('l3xz_head_ctrl')  + '/launch/head_ctrl.py'
     l3xz_head_ctrl = IncludeLaunchDescription(PythonLaunchDescriptionSource(l3xz_head_ctrl_launch_dir))
