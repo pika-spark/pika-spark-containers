2dc3b1128a80842bd446ce95f75a45d813e0edb9
Alexander Entinger <consulting@lxrobotics.com>
2023-09-27 07:36:58 +0200

    Fixing issue with venv path.

--- yakut-can/setup_yakut.sh
+++ yakut-can/setup_yakut.sh
@@ -8,6 +8,4 @@ export UAVCAN__CAN__MTU=8
 export UAVCAN__NODE__ID=$(yakut accommodate)  # Pick an unoccupied node-ID automatically for this shell session.
 echo "Auto-selected node-ID for this session: $UAVCAN__NODE__ID"
 
-cd /opt/yakut
-source env/bin/activate
 yakut monitor
